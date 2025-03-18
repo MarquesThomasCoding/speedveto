8b9fa2bc552d1ebad454ff6dd5a547296c219c53
Roland HUON <huonroland91@gmail.com>
2025-03-18 14:42:48 +0100

    Ajout de l'annotation ApiResource à l'entité Consultation pour l'intégration avec API Platform

--- src/Entity/Consultation.php
+++ src/Entity/Consultation.php
@@ -8,7 +8,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource()]
 #[ORM\Entity(repositoryClass: ConsultationRepository::class)]
 class Consultation
 {
