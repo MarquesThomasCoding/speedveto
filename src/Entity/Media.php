6e459b227695a4ea4bc2b91ddb3d0faa318354f5
Thomas Marques <thomas.marques@edu.devinci.fr>
2025-03-22 16:29:06 +0100

    Add media management with controller, normalizer and serializer

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -4,15 +4,59 @@ namespace App\Entity;
 
 use App\Repository\MediaRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\OpenApi\Model;
+use ApiPlatform\Metadata\ApiProperty;
+use App\Controller\CreateMediaObjectAction;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use ApiPlatform\Metadata\ApiResource;
-
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Attribute\Groups;
+ 
+#[Vich\Uploadable]
+#[ApiResource(
+    forceEager: false,
+    normalizationContext: ['groups' => ['read']],
+    types: ['https://schema.org/MediaObject'],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Delete(),
+        new Post(
+            controller: CreateMediaObjectAction::class,
+            deserialize: false,
+            validationContext: ['groups' => ['Default', 'write']],
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                )
+            )
+        )
+    ]
+)]
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
-#[ApiResource()]
 class Media
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255, nullable: true)]
@@ -20,6 +64,14 @@ class Media
 
     #[ORM\OneToOne(mappedBy: 'photo', cascade: ['persist', 'remove'])]
     private ?Animal $animal = null;
+
+    #[ApiProperty(types: ['https://schema.org/contentUrl'])]
+    #[Groups(['read'])]
+    public ?string $contentUrl = null;
+
+    #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath')]
+    #[Assert\NotNull(groups: ['write'])]
+    public ?File $file = null;
 
     public function getId(): ?int
     {
